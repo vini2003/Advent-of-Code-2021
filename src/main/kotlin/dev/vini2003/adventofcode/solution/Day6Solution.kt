98a863a27ef736e934382aa26a2b75df13533402
vini2003 <personal@vini2003.dev>
2021-12-07 10:57:32 -0300

    feat: improve day 6

--- src/main/kotlin/dev/vini2003/adventofcode/solution/Day6Solution.kt
+++ src/main/kotlin/dev/vini2003/adventofcode/solution/Day6Solution.kt
@@ -7,55 +7,50 @@ import kotlin.math.max
 object Day6Solution : Solution(2021, 6, 1) {
 	val NumberRegex = "([0-9]+)".toRegex()
 	
-	override fun solvePart1(input: URL): String {
-		val initialAges = NumberRegex.findAll(input.readText()).map(MatchResult::value).map(String::toInt)
+	class Population(line: String) {
+		private val ages = (0L..8L).associateWith { 0L }.toMutableMap()
 		
-		val ages = (0..8).associateWith { 0 }.toMutableMap()
-		
-		initialAges.forEach { age ->
-			ages[age] = ages[age]!! + 1
-		}
-		
-		for (d in 0..79) {
-			val prevAge0 = ages[0]!!
+		init {
+			NumberRegex.findAll(line).map(MatchResult::value).map(String::toLong).forEach { age ->
+				ages[age] = ages[age]!! + 1
+			}
 			
-			ages[0] = ages[1]!!
-			ages[1] = ages[2]!!
-			ages[2] = ages[3]!!
-			ages[3] = ages[4]!!
-			ages[4] = ages[5]!!
-			ages[5] = ages[6]!!
-			ages[6] = ages[7]!! + prevAge0
-			ages[7] = ages[8]!!
-			ages[8] = prevAge0
 		}
 		
-		return "${ages.values.sum()}"
+		fun simulate(days: Int) {
+			for (d in 0 until days) {
+				val prevAge0 = ages[0]!!
+				
+				ages[0] = ages[1]!!
+				ages[1] = ages[2]!!
+				ages[2] = ages[3]!!
+				ages[3] = ages[4]!!
+				ages[4] = ages[5]!!
+				ages[5] = ages[6]!!
+				ages[6] = ages[7]!! + prevAge0
+				ages[7] = ages[8]!!
+				ages[8] = prevAge0
+			}
+		}
+		
+		fun total(): Long {
+			return ages.values.sum()
+		}
+	}
+	
+	override fun solvePart1(input: URL): String {
+		val population = Population(input.readText())
+		
+		population.simulate(80)
+		
+		return "${population.total()}"
 	}
 	
 	override fun solvePart2(input: URL): String {
-		val initialAges = NumberRegex.findAll(input.readText()).map(MatchResult::value).map(String::toLong)
+		val population = Population(input.readText())
 		
-		val ages = (0L..8L).associateWith { 0L }.toMutableMap()
+		population.simulate(256)
 		
-		initialAges.forEach { age ->
-			ages[age] = ages[age]!! + 1L
-		}
-		
-		for (d in 0L..255L) {
-			val prevAge0 = ages[0]!!
-			
-			ages[0] = ages[1]!!
-			ages[1] = ages[2]!!
-			ages[2] = ages[3]!!
-			ages[3] = ages[4]!!
-			ages[4] = ages[5]!!
-			ages[5] = ages[6]!!
-			ages[6] = ages[7]!! + prevAge0
-			ages[7] = ages[8]!!
-			ages[8] = prevAge0
-		}
-		
-		return "${ages.values.sum()}"
+		return "${population.total()}"
 	}
 }
